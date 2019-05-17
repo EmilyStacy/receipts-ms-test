1ceb31e768f44244807fd8b45e7ebad55ba96cc3
Divya Donepudi <Divya.Donepudi@aa.com>
2019-05-17 14:01:39 -0500

    Unit tests (#12)
    * added response headers
    
    * added unit tests for controller and repository

--- src/test/java/com/aa/fly/receipts/integration/controller/ReceiptControllerTest.java
+++ src/test/java/com/aa/fly/receipts/integration/controller/ReceiptControllerTest.java
@@ -1,76 +1,89 @@
-package com.aa.fly.receipts.controller;
+package com.aa.fly.receipts.integration.controller;
 
+import java.util.Date;
+
+import com.aa.fly.receipts.controller.ReceiptController;
 import com.aa.fly.receipts.domain.Receipt;
-import com.aa.fly.receipts.domain.SearchCriteria;
 import com.aa.fly.receipts.service.ReceiptService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.aa.fly.receipts.domain.SearchCriteria;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Date;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Created by 629874 on 5/17/2019.
+ * Created by 629874 on 5/7/2019.
  */
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(value = ReceiptController.class, excludeFilters = {@ComponentScan.Filter(
+      type = FilterType.REGEX, pattern = "com.aa.fly.receipts.config.*")} )
+@RunWith(SpringRunner.class)
 
 public class ReceiptControllerTest
 {
-   @Mock
-   private ReceiptService receiptService;
 
-   @InjectMocks
-   private ReceiptController controller;
+   @Autowired
+   private MockMvc mockMvc;
+
+   @MockBean
+   private ReceiptService receiptService;
 
 
    @Test
    public void findReceipt_byTicketNumber() throws Exception
    {
-      final SearchCriteria searchCriteria = getSearchCriteria();
-      final Receipt receiptExpected = getReceipt(searchCriteria);
 
-      when(receiptService.findReceipt(any())).thenReturn(receiptExpected);
 
-      Receipt receiptAcutal =  controller.getReceipts(searchCriteria);
+      when(receiptService.findReceipt(any())).thenReturn(getReceipt());
+      RequestBuilder request = MockMvcRequestBuilders
+            .post("/api/receipt")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(searchCriteriaJson()
+            );
 
-      assertThat(receiptAcutal.getFirstName()).isEqualToIgnoringCase(searchCriteria.getFirstName());
-      assertThat(receiptAcutal.getLastName()).isEqualToIgnoringCase(searchCriteria.getLastName());
-      assertThat(receiptAcutal.getReceiptTotal()).isEqualToIgnoringCase(receiptExpected.getReceiptTotal());
+      MvcResult mvcResult = mockMvc.perform(request)
+            .andExpect(status().isOk())
+            .andExpect(content().json("{\n" + "   \"firstName\": FirstName,\n"
+                  + "   \"lastName\": LastName,\n" + "   \"receiptTotal\": \"239.00\"\n" + "}"))
+            .andReturn();
+
+      Assert.assertTrue(mvcResult.getResponse().getContentAsString().contains("239.00"));
 
    }
 
-   public static Receipt getReceipt(SearchCriteria searchCriteria)
+   private Receipt getReceipt()
    {
       Receipt receipt = new Receipt();
       receipt.setReceiptTotal("239.00");
-      receipt.setLastName(searchCriteria.getLastName());
-      receipt.setFirstName(searchCriteria.getFirstName());
+      receipt.setLastName("LastName");
+      receipt.setFirstName("FirstName");
       return receipt;
    }
 
-   public static SearchCriteria getSearchCriteria() {
+   private String searchCriteriaJson()
+   {
+      return "{ \"ticketNumber\": \"" + "0012362111828" +"\"}";
+   }
+
+   private SearchCriteria getSearchCriteria() {
       SearchCriteria searchCriteria = new SearchCriteria();
       searchCriteria.setDepartureDate(new Date());
       searchCriteria.setFirstName("first");
--- src/test/java/com/aa/fly/receipts/integration/data/ReceiptRepositoryTest.java
+++ src/test/java/com/aa/fly/receipts/integration/data/ReceiptRepositoryTest.java
@@ -1,43 +1,51 @@
-package com.aa.fly.receipts.data;
+package com.aa.fly.receipts.integration.data;
 
 import com.aa.fly.receipts.ReceiptsApplication;
+import com.aa.fly.receipts.data.ReceiptRepository;
 import com.aa.fly.receipts.domain.SearchCriteria;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.math.BigDecimal;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyObject;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 /**
- * Created by 629874 on 5/17/2019.
+ * Created by 629874 on 5/11/2019.
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ReceiptsApplication.class)
+
 public class ReceiptRepositoryTest
 {
-   @Mock
-   private JdbcTemplate jdbcTemplate;
-
-   @InjectMocks
+   @Autowired
    private ReceiptRepository receiptRepository;
+
+
 
    @Test
    public void findReceipt() {
       SearchCriteria criteria = new SearchCriteria();
       criteria.setTicketNumber("0012362111828");
-      when(jdbcTemplate.queryForObject(anyString(), any(), eq(String.class) )).thenReturn("251.2000");
+
       assertEquals("251.2000", receiptRepository.findReceipt(criteria).getReceiptTotal());
 
    }
