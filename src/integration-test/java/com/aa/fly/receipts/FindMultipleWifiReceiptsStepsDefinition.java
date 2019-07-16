6804075803b4fe2913b8ff221eb7e0d4c1e6bb4b
Jingchau Yang <Jingchau.Yang@aa.com>
2019-07-16 15:55:16 -0500

    Added Rest Assured capability for Integration Tests. (#37)
    * Added Rest Assured capability for Integration Tests.
    
    * Adjusted the tests.

---
+++ src/integration-test/java/com/aa/fly/receipts/FindMultipleWifiReceiptsStepsDefinition.java
@@ -0,0 +1,59 @@
+package com.aa.fly.receipts;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+import java.text.ParseException;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+
+public class FindMultipleWifiReceiptsStepsDefinition {
+
+    private static String REQUEST_PAYLOAD;
+    private ValidatableResponse validatableResponse;
+
+    @Given("^I want to find multiple wifi receipts$")
+    public void i_want_to_find_multiple_wifi_receipts() throws Throwable {
+
+    }
+
+    @When("^I search with last name \"([^\"]*)\", lastFourOfCreditCard \"([^\"]*)\", smaller start date \"([^\"]*)\", end date \"([^\"]*)\"$")
+    public void i_search_with_last_name_last_OfCreditCard_smaller_start_date_end_date(String lastName, String last4OfCreditCard, String startDate, String endDate) throws ParseException {
+
+        REQUEST_PAYLOAD = new StringBuffer("{\n")
+                .append(" \"lastName\": \"").append(lastName).append("\",\n")
+                .append(" \"ccLastFour\": \"").append(last4OfCreditCard).append("\",\n")
+                .append(" \"startDate\": \"").append(startDate).append("\",\n")
+                .append(" \"endDate\": \"").append(endDate).append("\"\n")
+                .append("}").toString();
+    }
+
+    @Then("^I get a response$")
+    public void i_get_a_response() throws Throwable {
+
+        RestAssured.baseURI = System.getProperty("branch.application.url");
+
+        validatableResponse = given()
+                .contentType(ContentType.JSON)
+                .body(REQUEST_PAYLOAD)
+                .post("/api/wifi-receipt")
+                .then();
+    }
+
+    @Then("^I get a successful multiple wifi receipts response$")
+    public void i_get_a_successful_multiple_wifi_receipts_response() throws Throwable {
+
+        validatableResponse.statusCode(200);
+    }
+
+    @Then("^I found two records$")
+    public void i_found_two_records() throws Throwable {
+
+        validatableResponse.body("wifiLineItems.orderId", hasItems("0123456789AA", "0123456790AA"));
+    }
+}
--- src/integration-test/resources/com/aa/fly/receipts/findWifiReceipts.feature
+++ src/integration-test/resources/com/aa/fly/receipts/findWifiReceipts.feature
@@ -4,14 +4,27 @@ Feature: Search wifi subscriptions by CC last four, lastName and purchase date r
 	I want to search my wifi subscriptions
 	So that I can display them
 
-Scenario Outline: Happy path scenario
+    Scenario Outline: Happy path scenario
+    
+      Given I want to find my wifi subscription
+      When I search with CC last four digits "<ccLastFour>", last name "<lastName>", start date "<startDate>", end date "<endDate>"
+      Then I get a successful response
+      Then I find number of subscription records "<noOfRecords>"
+    
+      Examples:
+      | ccLastFour | lastName  | startDate  | endDate       | noOfRecords  |
+      | 1234       | SMITH     | 2019-04-01 | 2019-05-01    | 1            |
+      | 1234       | SMITH     | 2019-04-01 | 2019-06-01    | 2            |
 
-  Given I want to find my wifi subscription
-  When I search with CC last four digits "<ccLastFour>", last name "<lastName>", start date "<startDate>", end date "<endDate>"
-  Then I get a successful response
-  Then I find number of subscription records "<noOfRecords>"
+  Scenario Outline: Search multiple wifi receipts
 
-  Examples:
-  | ccLastFour | lastName  | startDate  | endDate       | noOfRecords  |
-  | 1234       | SMITH     | 2019-04-01 | 2019-05-01    | 1            |
-  | 1234       | SMITH     | 2019-04-01 | 2019-06-01    | 2            |
+    Given I want to find multiple wifi receipts
+    When I search with last name "<lastName>", lastFourOfCreditCard "<lastFourOfCreditCard>", smaller start date "<startDate>", end date "<endDate>"
+    Then I get a response
+    Then I get a successful multiple wifi receipts response
+    Then I found two records
+
+    Examples:
+      | lastName | lastFourOfCreditCard | startDate                | endDate                  |
+      | Smith    | 1234                 | 2019-02-01T17:21:46.932Z | 2019-06-01T17:21:46.932Z | 
+  
