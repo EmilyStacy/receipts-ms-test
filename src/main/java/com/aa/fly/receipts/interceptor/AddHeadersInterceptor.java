e352742a36f321ae50e9fd40595c2864d5cec44e
Priyanka <priyanka.tilak@aa.com>
2020-03-03 09:59:39 -0600

    Logging update for transaction ID

--- src/main/java/com/aa/fly/receipts/interceptor/AddHeadersInterceptor.java
+++ src/main/java/com/aa/fly/receipts/interceptor/AddHeadersInterceptor.java
@@ -3,6 +3,7 @@ package com.aa.fly.receipts.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -15,6 +16,7 @@ public class AddHeadersInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+        MDC.put("XTransactionID", request.getHeader("X-Transaction-ID"));
         response.addHeader("X-Transaction-ID", request.getHeader("X-Transaction-ID"));
         return true;
     }
