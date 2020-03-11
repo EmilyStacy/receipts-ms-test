44c9e282b3e3f8d8df65924f6ec24b2fd37c2f62
Divya Donepudi <divya.donepudi@aa.com>
2020-03-11 15:24:57 -0500

    removed unused imports

--- src/main/java/com/aa/fly/receipts/controller/TicketReceiptController.java
+++ src/main/java/com/aa/fly/receipts/controller/TicketReceiptController.java
@@ -3,8 +3,6 @@
  */
 package com.aa.fly.receipts.controller;
 
-import java.net.SocketTimeoutException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
