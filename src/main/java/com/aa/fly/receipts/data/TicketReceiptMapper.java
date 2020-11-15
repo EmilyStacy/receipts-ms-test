a50f85cedac7bbb2fd4304fea993771e3b1a2d6d
854495 <854495@host.docker.internal>
2020-11-15 12:10:53 -0600

    Added commented code for FOP building.

--- src/main/java/com/aa/fly/receipts/data/TicketReceiptMapper.java
+++ src/main/java/com/aa/fly/receipts/data/TicketReceiptMapper.java
@@ -1,8 +1,13 @@
 package com.aa.fly.receipts.data;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +17,7 @@ import com.aa.fly.receipts.data.builder.PassengerBuilder;
 import com.aa.fly.receipts.data.builder.PassengerFareTaxFeeBuilder;
 import com.aa.fly.receipts.data.builder.PnrHeaderBuilder;
 import com.aa.fly.receipts.data.builder.PnrSegmentBuilder;
+import com.aa.fly.receipts.domain.FormOfPayment;
 import com.aa.fly.receipts.domain.FormOfPaymentKey;
 import com.aa.fly.receipts.domain.TicketReceipt;
 import com.aa.fly.receipts.domain.TicketReceiptRsRow;
@@ -36,6 +42,8 @@ public class TicketReceiptMapper {
         String firstDepartureDateTime = null;
         String lastDepartureDateTime = null;
         String currentDepartureDateTime = null;
+        Set<FormOfPaymentKey> fopKeys = new HashSet<>();
+        List<FormOfPayment> formOfPayments = new ArrayList<>();
 
         Iterator<TicketReceiptRsRow> iterator = ticketReceiptRsRowList.iterator();
         TicketReceiptRsRow ticketReceiptRsRow = null;
@@ -64,14 +72,24 @@ public class TicketReceiptMapper {
             
         	// Building data from every row in the first segment.
             if (firstDepartureDateTime.equals(currentDepartureDateTime)) {
+            	// FOP building setup
+	            //FormOfPaymentKey formOfPaymentKey = new FormOfPaymentKey(
+	            		//ticketReceiptRsRow.getFopSeqId(), ticketReceiptRsRow.getFopTypeCd());
+	            
+                //if (!fopKeys.contains(formOfPaymentKey) && isMappingFormOfPayment(ticketReceiptRsRow.getFopTypeCd())) {
+                	// build FOPs
+                	// ticketReceiptReturn = passengerFopBuilder.build(ticketReceiptReturn, ticketReceiptRsRow);
+
+                    //this.adjustFormOfPaymentsIfExchanged(ticketReceiptReturn.getPassengerDetails().get(0).getFormOfPayments());
+    	            //fopKeys.add(formOfPaymentKey);
+                //}
+                
             	// Build Tax Item (Set).
-            	// build FOPs --> based on, FormOfPaymentKey formOfPaymentKey = new FormOfPaymentKey(fopSequenceId, fopTypeCode);
             	// Build Ancillary (Set) -> Add to set anclryDocNums.add(anclryDocNbr);
             	// if (anclryDocNbr not in anclryDocNums)
             	//   Build Ancillary FOPs.
             }
-            
-            
+           
         	// Building data from the row when segment changed.
             if (!lastDepartureDateTime.equals(currentDepartureDateTime))
             {
@@ -90,4 +108,20 @@ public class TicketReceiptMapper {
         
         return ticketReceiptReturn;
     }
+    
+    // Move to PassengerFopBuilder
+    private boolean isMappingFormOfPayment(String fopTypeCode) {
+        return fopTypeCode.startsWith("CC") || fopTypeCode.startsWith("CA");
+    }
+    
+    private List<FormOfPayment> adjustFormOfPaymentsIfExchanged(List<FormOfPayment> formOfPayments) {
+        boolean isExchange = formOfPayments.stream().anyMatch(f -> "EF".equals(f.getFopTypeCode()) || "EX".equals(f.getFopTypeCode()));
+        if (isExchange) {
+            formOfPayments = formOfPayments.stream().filter(f -> f.getFopAmount() != null && BigDecimal.valueOf(Double.valueOf(f.getFopAmount())).compareTo(BigDecimal.ZERO) > 0)
+                    .collect(Collectors.toList());
+            formOfPayments.stream().forEach(f -> f.setFopTypeDescription("Exchange - " + f.getFopTypeDescription()));
+        }
+
+        return formOfPayments;
+    }    
 }
