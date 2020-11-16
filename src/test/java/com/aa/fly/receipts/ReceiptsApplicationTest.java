78016f7d40439ba103172e63e24d245c6b1b8ce0
Sandesh Neupane <sandesh.neupane@aa.com>
2020-11-16 11:53:13 -0600

    fixed test failure

--- src/test/java/com/aa/fly/receipts/ReceiptsApplicationTest.java
+++ src/test/java/com/aa/fly/receipts/ReceiptsApplicationTest.java
@@ -1,19 +1,39 @@
 package com.aa.fly.receipts;
 
+import com.aa.fly.receipts.config.AppConfig;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
 
 @RunWith( SpringJUnit4ClassRunner.class )
 @SpringBootTest
 public class ReceiptsApplicationTest {
+
+   @InjectMocks
+   private ReceiptsApplication receiptsApplication;
+   @Mock
+   private AppConfig appConfig;
+   @Mock
+   private JdbcTemplate jdbcTemplate;
+   @Mock
+   private DataSource dataSource;
 	
    @Test
    public void ensureApplicationStarts()
    {
-      ReceiptsApplication.main( new String[] {} );
+      Mockito.when(appConfig.jdbcTemplateTicketReceipts(null)).thenReturn(jdbcTemplate);
+      Mockito.when(appConfig.jdbcTemplateWifi(null)).thenReturn(jdbcTemplate);
+      Mockito.when(appConfig.dataSourceTicketReceipts()).thenReturn(dataSource);
+      Mockito.when(appConfig.dataSourceWifi()).thenReturn(dataSource);
+      receiptsApplication.main( new String[] {} );
       Assert.assertNotNull( ReceiptsApplication.class );
    }
 }
