d8bdcc1dc49b0db4d5b1883a5b592fcec7806e25
854495 <854495@host.docker.internal>
2020-11-17 09:09:56 -0600

    Added more tests

--- src/test/java/com/aa/fly/receipts/data/adjuster/PassengerTotalAdjusterTest.java
+++ src/test/java/com/aa/fly/receipts/data/adjuster/PassengerTotalAdjusterTest.java
@@ -91,4 +91,79 @@ public class PassengerTotalAdjusterTest {
     	assertEquals("462.80", ticketReceiptReturn.getPassengerDetails().get(0).getPassengerTotalAmount());
     	assertEquals(false, ticketReceiptReturn.getPassengerDetails().get(0).isShowPassengerTotal());
 	}
+
+	@Test
+	public void testAdjustTicketFopPlusTwoOtherFopsWithSameCurrency() throws ParseException {
+    	ticketReceiptMock = Utils.mockTicketReceipt();
+    	
+        FormOfPayment formOfPayment = new FormOfPayment();
+        formOfPayment.setFopIssueDate(Constants.dateFormat.parse("2020-10-12"));
+        formOfPayment.setFopTypeCode(Constants.FOP_TYPE_CODE);
+        formOfPayment.setFopTypeDescription(Constants.FOP_TYPE_DESCRIPTION);
+        formOfPayment.setFopAccountNumberLast4(Constants.FOP_ACCOUNT_NUMBER_LAST4);
+        formOfPayment.setFopAmount("10.00");
+        formOfPayment.setFopCurrencyCode(Constants.FOP_CURRENCY_CODE);
+    	
+        Utils.addOneAncillary(formOfPayment);
+
+    	ticketReceiptMock.getPassengerDetails().get(0).getFormOfPayments().add(formOfPayment);
+    	
+        formOfPayment = new FormOfPayment();
+        formOfPayment.setFopIssueDate(Constants.dateFormat.parse("2020-10-14"));
+        formOfPayment.setFopTypeCode(Constants.FOP_TYPE_CODE);
+        formOfPayment.setFopTypeDescription(Constants.FOP_TYPE_DESCRIPTION);
+        formOfPayment.setFopAccountNumberLast4(Constants.FOP_ACCOUNT_NUMBER_LAST4);
+        formOfPayment.setFopAmount("20.00");
+        formOfPayment.setFopCurrencyCode(Constants.FOP_CURRENCY_CODE);
+    	
+        Utils.addOneAncillary(formOfPayment);
+
+    	ticketReceiptMock.getPassengerDetails().get(0).getFormOfPayments().add(formOfPayment);
+    	
+    	ticketReceiptReturn = passengerTotalAdjuster.adjust(ticketReceiptMock);
+    	
+    	assertEquals(Constants.FOP_AMOUNT, ticketReceiptReturn.getPassengerDetails().get(0).getFormOfPayments().get(0).getFopAmount());
+    	assertEquals("10.00", ticketReceiptReturn.getPassengerDetails().get(0).getFormOfPayments().get(1).getFopAmount());
+    	assertEquals("20.00", ticketReceiptReturn.getPassengerDetails().get(0).getFormOfPayments().get(2).getFopAmount());
+    	assertEquals("482.80", ticketReceiptReturn.getPassengerDetails().get(0).getPassengerTotalAmount());
+    	assertEquals(true, ticketReceiptReturn.getPassengerDetails().get(0).isShowPassengerTotal());
+	}
+	
+	@Test
+	public void testAdjustTicketFopPlusTwoOtherFopsWithDiffCurrency() throws ParseException {
+    	ticketReceiptMock = Utils.mockTicketReceipt();
+    	ticketReceiptMock.getPassengerDetails().get(0).getFareTaxesFees().setBaseFareCurrencyCode("EUR");;
+    	
+        FormOfPayment formOfPayment = new FormOfPayment();
+        formOfPayment.setFopIssueDate(Constants.dateFormat.parse("2020-10-12"));
+        formOfPayment.setFopTypeCode(Constants.FOP_TYPE_CODE);
+        formOfPayment.setFopTypeDescription(Constants.FOP_TYPE_DESCRIPTION);
+        formOfPayment.setFopAccountNumberLast4(Constants.FOP_ACCOUNT_NUMBER_LAST4);
+        formOfPayment.setFopAmount("10.00");
+        formOfPayment.setFopCurrencyCode("USD");
+        
+        Utils.addOneAncillary(formOfPayment);
+    	
+    	ticketReceiptMock.getPassengerDetails().get(0).getFormOfPayments().add(formOfPayment);
+    	
+        formOfPayment = new FormOfPayment();
+        formOfPayment.setFopIssueDate(Constants.dateFormat.parse("2020-10-12"));
+        formOfPayment.setFopTypeCode(Constants.FOP_TYPE_CODE);
+        formOfPayment.setFopTypeDescription(Constants.FOP_TYPE_DESCRIPTION);
+        formOfPayment.setFopAccountNumberLast4(Constants.FOP_ACCOUNT_NUMBER_LAST4);
+        formOfPayment.setFopAmount("20.00");
+        formOfPayment.setFopCurrencyCode("USD");
+        
+        Utils.addOneAncillary(formOfPayment);
+    	
+    	ticketReceiptMock.getPassengerDetails().get(0).getFormOfPayments().add(formOfPayment);
+    	
+    	ticketReceiptReturn = passengerTotalAdjuster.adjust(ticketReceiptMock);
+    	
+    	assertEquals(Constants.FOP_AMOUNT, ticketReceiptReturn.getPassengerDetails().get(0).getFormOfPayments().get(0).getFopAmount());
+    	assertEquals("10.00", ticketReceiptReturn.getPassengerDetails().get(0).getFormOfPayments().get(1).getFopAmount());
+    	assertEquals("20.00", ticketReceiptReturn.getPassengerDetails().get(0).getFormOfPayments().get(2).getFopAmount());
+    	assertEquals("482.80", ticketReceiptReturn.getPassengerDetails().get(0).getPassengerTotalAmount());
+    	assertEquals(false, ticketReceiptReturn.getPassengerDetails().get(0).isShowPassengerTotal());
+	}	
 }
