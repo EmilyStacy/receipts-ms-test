0b97ecac924eb53e24b6558c775510f29ea45d29
Shabarinath Dodda <Shabarinath.Dodda@aa.com>
2020-12-07 09:56:12 -0600

    changes to anclrydocum to print 13 digits includes all test chanegs

--- src/main/java/com/aa/fly/receipts/data/builder/PassengerAncillaryBuilder.java
+++ src/main/java/com/aa/fly/receipts/data/builder/PassengerAncillaryBuilder.java
@@ -1,13 +1,12 @@
 package com.aa.fly.receipts.data.builder;
 
 import com.aa.fly.receipts.domain.Ancillary;
-import com.aa.fly.receipts.domain.FormOfPayment;
-import com.aa.fly.receipts.domain.TicketReceipt;
 import com.aa.fly.receipts.domain.TicketReceiptRsRow;
-import com.aa.fly.receipts.service.DataBuilderService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
@@ -17,16 +16,28 @@ import java.util.Set;
 @Component
 public class PassengerAncillaryBuilder {
 
+    public static String ancillaryDocNumberCountNine = "0010";
+    public static String ancillaryDocNumberCountTen = "001";
+
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    public Set<Ancillary> build( TicketReceiptRsRow ticketReceiptRsRow ) {
+
+    public Set<Ancillary> build(TicketReceiptRsRow ticketReceiptRsRow) {
 
         String ancillaryDocNumber = ticketReceiptRsRow.getAnclryDocNbr();
         Set<Ancillary> ancillaryList = new HashSet<>();
 
         if (StringUtils.isNotBlank(ancillaryDocNumber)) {
             Ancillary ancillary = new Ancillary();
-            ancillary.setAnclryDocNbr(ancillaryDocNumber);
+            int ancillaryDocNumberCount = ancillaryDocNumber.length();
+            if (ancillaryDocNumberCount == 9) {
+                ancillaryDocNumber = ancillaryDocNumberCountNine.concat(ancillaryDocNumber);
+                ancillary.setAnclryDocNbr(ancillaryDocNumber);
+            } else if (ancillaryDocNumberCount == 10) {
+                ancillaryDocNumber = ancillaryDocNumberCountTen.concat(ancillaryDocNumber);
+                ancillary.setAnclryDocNbr(ancillaryDocNumber);
+            }
+
             ancillary.setAnclryIssueDate(ticketReceiptRsRow.getAnclryIssueDt() != null ? dateFormat.format(ticketReceiptRsRow.getAnclryIssueDt()) : "");
             ancillary.setAnclryProdCode(ticketReceiptRsRow.getAnclryProdCd());
 
@@ -46,9 +57,12 @@ public class PassengerAncillaryBuilder {
             String anclrySalesCurrencyAmount = ticketReceiptRsRow.getAnclrySlsCurncyAmt();
             ancillary.setAnclrySalesCurrencyAmount(anclrySalesCurrencyAmount);
             ancillary.setAnclrySalesCurrencyCode(ticketReceiptRsRow.getAnclrySlsCurncyCd());
+            if (ancillaryPriceCurrencyAmount != null) {
+                BigDecimal anclryTaxCurrencyAmount = new BigDecimal(anclrySalesCurrencyAmount).subtract(new BigDecimal(ancillaryPriceCurrencyAmount)).setScale(2, RoundingMode.CEILING);
+                ancillary.setAnclryTaxCurrencyAmount(anclryTaxCurrencyAmount.toString());
+            }
 
-            BigDecimal anclryTaxCurrencyAmount = new BigDecimal(anclrySalesCurrencyAmount).subtract(new BigDecimal(ancillaryPriceCurrencyAmount)).setScale(2, RoundingMode.CEILING);
-            ancillary.setAnclryTaxCurrencyAmount(anclryTaxCurrencyAmount.toString());
+
             ancillaryList.add(ancillary);
         }
         return ancillaryList;
