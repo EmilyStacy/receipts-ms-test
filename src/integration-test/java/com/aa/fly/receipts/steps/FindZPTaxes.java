fcca7622519eb7e72a18f222b88b81e247b28a34
Shabarinath Dodda <Shabarinath.Dodda@aa.com>
2021-01-08 10:19:49 -0600

    refactore code

--- src/integration-test/java/com/aa/fly/receipts/steps/FindZPTaxes.java
+++ src/integration-test/java/com/aa/fly/receipts/steps/FindZPTaxes.java
@@ -2,11 +2,9 @@ package com.aa.fly.receipts.steps;
 
 import com.aa.fly.receipts.SpringIntegrationSetup;
 import com.aa.fly.receipts.domain.FareTaxesFees;
-import com.aa.fly.receipts.domain.SearchCriteriaApi2;
 import com.aa.fly.receipts.domain.TicketReceipt;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import gherkin.deps.com.google.gson.Gson;
 import gherkin.deps.com.google.gson.GsonBuilder;
 import org.junit.Assert;
@@ -15,7 +13,6 @@ import java.text.SimpleDateFormat;
 import java.util.stream.Collectors;
 
 public class FindZPTaxes extends SpringIntegrationSetup {
-    private SearchCriteriaApi2 criteriaApi2 = new SearchCriteriaApi2();
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @Given("^I want to retrieve ZP taxes details for scenario \"(.+)\"$")
