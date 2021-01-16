cf5d8396ee37aa359e9530f28836d8c36b1fef11
854495 <jingchau.yang@aa.com>
2021-01-16 10:07:31 -0600

    Added UTs and Mockito for Junit5.

--- src/test/java/com/aa/fly/receipts/manager/AgencyTicketManagerTest.java
+++ src/test/java/com/aa/fly/receipts/manager/AgencyTicketManagerTest.java
@@ -1,62 +1,55 @@
 package com.aa.fly.receipts.manager;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import com.aa.fly.receipts.data.TicketViewRepository;
 import com.aa.fly.receipts.exception.AgencyTicketException;
 
+@ExtendWith(MockitoExtension.class)
 class AgencyTicketManagerTest {
-	
-	private AgencyTicketManager agencyTicketManager = new AgencyTicketManager();
-
-	@BeforeEach
-	void setup() {
-		List<String> formCodes = new ArrayList<>();
-		formCodes.add("21");
-		formCodes.add("23");
-		formCodes.add("24");
-		agencyTicketManager.setNonAgencyFormCodes(formCodes);
-	}
-	
-	@Test
-	void testGetNonAgencyFormCodes() throws Exception {
-		Assertions.assertEquals("[21, 23, 24]", agencyTicketManager.getNonAgencyFormCodes().toString());
-	}
+    @Mock
+    private TicketViewRepository ticketViewRepository;
+    
+    @Mock
+    private SqlRowSet sqlRowSet;
+    
+    @InjectMocks
+    private AgencyTicketManager agencyTicketManager;
 	
 	@Test
 	void testCheckThrowsAgencyTicketException() throws Exception {
+		
+		Mockito.when(ticketViewRepository.findTicketViewByTicketNumber(Mockito.anyString()))
+        .thenReturn(sqlRowSet);
+		
+		Mockito.when(sqlRowSet.next())
+        .thenReturn(true);
+		
 		Assertions.assertThrows(AgencyTicketException.class, 
 				() -> {
-					agencyTicketManager.check("0000000000");
-				});
-		Assertions.assertThrows(AgencyTicketException.class, 
-				() -> {
-					agencyTicketManager.check("2200000000");
-				});
-		Assertions.assertThrows(AgencyTicketException.class, 
-				() -> {
-					agencyTicketManager.check("2500000000");
+					agencyTicketManager.check("");
 				});
 	}
 
 	@Test
 	void testCheckNotThrowsAgencyTicketException() throws Exception {
-		Assertions.assertDoesNotThrow( 
-				() -> {
-					agencyTicketManager.check("2100000000");
-				});
-		Assertions.assertDoesNotThrow( 
-				() -> {
-					agencyTicketManager.check("2300000000");
-				});
+		
+		Mockito.when(ticketViewRepository.findTicketViewByTicketNumber(Mockito.anyString()))
+        .thenReturn(sqlRowSet);
+		
+		Mockito.when(sqlRowSet.next())
+        .thenReturn(false);
+
 		Assertions.assertDoesNotThrow(
 				() -> {
-					agencyTicketManager.check("2400000000");
+					agencyTicketManager.check("");
 				});
 	}
-	
 }
